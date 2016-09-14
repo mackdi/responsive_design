f2af5ad361739e8936e477c2204141a9094cb207
mackdi <dinkokovachev@gmail.com>
2016-09-14 18:32:49 +0300

    added login
    added modal login & button

---
+++ login.php
@@ -0,0 +1,27 @@
+<!-- The Modal -->
+<div id="id01" class="modal">
+  
+
+  <!-- Modal Content -->
+  <form class="modal-content animate" action="action_page.php">
+    
+
+    <div class="container-modal_box">
+      <label><b>Username</b></label>
+      <input class="modal_box" type="text" placeholder="Enter Username" name="uname" required>
+
+      <label><b>Password</b></label>
+      <input class="modal_box" type="password" placeholder="Enter Password" name="psw" required>
+
+      <button  class="modal_box" type="submit">Login</button>
+      <input  class="modal_box" type="checkbox" id="check"> Remember me
+    </div>
+
+    <div class="container-modal_box" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" id="cancel-btn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+
+
