a3680f2c03b452632302a1930ba46d6827e9f63b
mackdi <dinkokovachev@gmail.com>
2016-09-18 21:28:32 +0300

    RESPONSIVE LAYOUT FINISHED
    PHP FORM S IN WORK

--- server.php
+++ server.php
@@ -16,13 +16,26 @@ catch (PDOExeption $e) {
 if(!empty($_POST['uname']) && !empty($_POST['psw'])):
   $sql = "INSERT INTO users(username ,password) VALUES(:username , :password)";
   $stmt = $conn->prepare($sql);
-
   $stmt->bindParam(':username', $_POST['uname']);
-  $stmt->bindParam(':password', password_hash($_POST['psw'], PASSWORD_BCRYPT));
+  $stmt->bindParam(':password', $_POST['psw']);
   if($stmt->execute()) :
-    die('Success');
+    die('Logged in!');
   else:
-    die('Fail');
+    die('Failed');
 	endif;
+endif;
+
+
+//subscribers to db
+if(!empty($_POST['sb-name']) && !empty($_POST['sb-email'])):
+  $sql = "INSERT INTO subscribers(name ,email) VALUES(:name , :email)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':name', $_POST['sb-name']);
+  $stmt->bindParam(':email', $_POST['sb-email']);
+  if($stmt->execute()) :
+    die('Subscribed!');
+  else:
+    die('Failed');
+  endif;
 endif;
 ?>
